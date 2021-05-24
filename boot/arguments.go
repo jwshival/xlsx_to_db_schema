7f2293d54d69072fbae2cd949e02febd2d9ba7d2
Hoang Pham Dang <phamdanghoang.68@gmail.com>
2021-05-24 14:04:39 +0900

    Refactor code

--- boot/arguments.go
+++ boot/arguments.go
@@ -5,9 +5,8 @@ import (
 	"xlsx_to_db_schema/constants"
 )
 
-
 func InitArguments(adapter *string, sourcePath *string, destPath *string) {
-	flag.StringVar(*&adapter, constants.MySQLAdapter, constants.MySQLAdapter, constants.AdapterName)
+	flag.StringVar(adapter, constants.AdapterName, constants.MySQLAdapter, constants.AdapterName)
 	flag.StringVar(sourcePath, constants.SourcePathName, constants.SourcePathDefault, constants.SourcePathName)
 	flag.StringVar(destPath, constants.DestPathName, constants.DestPathDefault, constants.DestPathName)
 	flag.Parse()
--- constants/config.go
+++ constants/config.go
@@ -1,15 +1,17 @@
 package constants
 
 const (
-	AdapterName        string = "adapter"
-	MySQLAdapter       string = "mysql"
-	PSQLAdapter        string = "postgre"
-	FilePathNotExist   string = "File path is not exist"
-	FileExtensionWrong string = "File extension is wrong"
-	FileHeaderWrong    string = "File header is wrong"
-	ExecuteDone        string = "Done!"
-	SourcePathName     string = "source"
-	SourcePathDefault  string = "/"
-	DestPathName       string = "des"
-	DestPathDefault    string = "/"
+	AdapterName       string = "adapter"
+	MySQLAdapter      string = "mysql"
+	PSQLAdapter       string = "postgre"
+	ExecuteDone       string = "Done!"
+	SourcePathName    string = "source"
+	SourcePathDefault string = "/"
+	DestPathName      string = "dest"
+	DestPathDefault   string = "/"
+	// Errors
+	NotSupportAdapter  string = "no support this adapter"
+	FilePathNotExist   string = "file path is not exist"
+	FileExtensionWrong string = "file extension is wrong"
+	FileHeaderWrong    string = "file header is wrong"
 )
